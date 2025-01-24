d36fd6e33dd970403e7dd85245a518a7ecfd7e1d
divyeshravi30 <divyeshravi30@gmail.com>
2025-01-24 22:43:07 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,13 @@
+public class even
+ {
+   public static void main (String args[])
+    {
+      int limit = 50;
+      System.out.println("Displaying even numbers between 1 to 50"+limit);
+      for (int i = 1; i <= limit; i++)
+       
+          if(i%2 == 0)
+	  System.out.print(i + " ");
+        
+    }
+  }
